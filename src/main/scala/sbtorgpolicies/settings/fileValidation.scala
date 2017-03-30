4fd63cc4225a859deae514a05805f89c2dd0705c
Juan Pedro Moreno <juanpmorest@gmail.com>
2017-03-30 17:46:50 +0200

    Changes ad-hoc path by its val

--- src/main/scala/sbtorgpolicies/settings/fileValidation.scala
+++ src/main/scala/sbtorgpolicies/settings/fileValidation.scala
@@ -20,7 +20,7 @@ import cats.data.Validated.{Invalid, Valid}
 import sbt.Keys._
 import sbt._
 import sbtorgpolicies.exceptions.ValidationException
-import sbtorgpolicies.model.{Dev, License}
+import sbtorgpolicies.model.Dev
 import sbtorgpolicies.rules._
 
 trait fileValidationKeys {
@@ -54,7 +54,7 @@ trait fileValidation extends fileValidationKeys with ValidationFunctions with ke
         getChildPath(baseDirectory.value, "NOTICE.md"),
         List(requiredStrings(List(name.value, orgLicenseSetting.value.name)))
       ),
-      mkValidation(getChildPath(baseDirectory.value, "version.sbt"), List(emptyValidation))
+      mkValidation(getChildPath(baseDirectory.value, sbtorgpolicies.templates.versionFilePath), List(emptyValidation))
     )
   )
 
