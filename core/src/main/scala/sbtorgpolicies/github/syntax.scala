065e438279f29db57e39e6464bbed84055504cb2
Juan Pedro Moreno <juanpmorest@gmail.com>
2017-04-03 23:04:12 +0200

    Improves GithubOps syntax methods

--- core/src/main/scala/sbtorgpolicies/github/syntax.scala
+++ core/src/main/scala/sbtorgpolicies/github/syntax.scala
@@ -3,41 +3,42 @@ package sbtorgpolicies.github
 import cats.data.EitherT
 import github4s.Github._
 import github4s.jvm.Implicits._
-import github4s.GithubResponses.{GHException, GHIO, GHResponse, GHResult}
+import github4s.GithubResponses._
 import sbtorgpolicies.exceptions.GitHubException
 import sbtorgpolicies.github.instances._
 
+import scala.language.implicitConversions
 import scala.util.{Failure, Success, Try}
 import scalaj.http.HttpResponse
 
 object syntax {
 
-  private[this] val commonHeaders = Map("user-agent" -> "sbt-org-policies")
+  type EitherTGHResult[A] = EitherT[GHIO, GHException, GHResult[A]]
 
-  implicit class EitherTOps[R](eitherT: EitherT[GHIO, GHException, GHResult[R]]) {
+  implicit def ghEitherTSyntax[A](eitherT: EitherTGHResult[A]): EitherTOps[A] = new EitherTOps[A](eitherT)
 
-    def execE: Either[GitHubException, R] = eitherT.value.execE
+  implicit def ghResponseSyntax[A](ghResponse: GHIO[GHResponse[A]]): GHResponseOps[A] = new GHResponseOps(ghResponse)
+
+  implicit def ghResultSyntax[A](gHResult: GHResult[A]): GHResultOps[A] = new GHResultOps[A](gHResult)
+
+  final class EitherTOps[A](eitherT: EitherTGHResult[A]) {
+
+    def execE: Either[GitHubException, A] =
+      eitherT.value.execE
 
   }
 
-  implicit class GHIOOps[R](ghIO: GHIO[Either[GHException, GHResult[R]]]) {
+  final class GHResponseOps[A](ghResponse: GHIO[GHResponse[A]]) {
 
-    def execE: Either[GitHubException, R] =
-      ghIO.exec[Try, HttpResponse[String]](commonHeaders).toEither
+    val commonHeaders: Map[String, String] = Map("user-agent" -> "sbt-org-policies")
 
-  }
-
-  implicit class GHResponseTrySyntax[R](m: Try[GHResponse[R]]) {
-
-    def toEither: Either[GitHubException, R] = m match {
+    def execE: Either[GitHubException, A] = ghResponse.exec[Try, HttpResponse[String]](commonHeaders) match {
       case Success(Right(r)) => Right(r.result)
       case Success(Left(e))  => Left(GitHubException(s"GitHub returned an error: ${e.getMessage}", Some(e)))
       case Failure(e)        => Left(GitHubException("Error making request to GitHub", Some(e)))
     }
 
   }
-
-  implicit def ghResultSyntax[A](gHResult: GHResult[A]): GHResultOps[A] = new GHResultOps[A](gHResult)
 
   final class GHResultOps[A](gHResult: GHResult[A]) {
 
