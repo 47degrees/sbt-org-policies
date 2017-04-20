2bd8295607ce6601a5d7f63fcf1d2e0f82e54cca
Juan Pedro Moreno <juanpmorest@gmail.com>
2017-04-20 19:26:00 +0200

    Bumps some library versions

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -30,7 +30,7 @@ object ProjectPlugin extends AutoPlugin {
       addSbtPlugin("org.scoverage"      % "sbt-scoverage"          % "1.5.0"),
       addSbtPlugin("org.scala-js"       % "sbt-scalajs"            % "0.6.15"),
       addSbtPlugin("de.heikoseeberger"  % "sbt-header"             % "1.8.0"),
-      addSbtPlugin("com.eed3si9n"       % "sbt-buildinfo"          % "0.6.1"),
+      addSbtPlugin("com.eed3si9n"       % "sbt-buildinfo"          % "0.7.0"),
       addSbtPlugin("com.geirsson"       % "sbt-scalafmt"           % "0.6.8"),
       addSbtPlugin("com.47deg"          % "sbt-dependencies"       % "0.1.0"),
       addSbtPlugin("com.47deg"          % "sbt-microsites"         % "0.5.3")
