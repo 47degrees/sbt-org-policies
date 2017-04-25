99a58bf027ae72b6a74e7cb134924068afcb3b8c
Juan Pedro Moreno <juanpmorest@gmail.com>
2017-04-25 12:40:17 +0200

    Adds optional publishMicrosite

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -33,7 +33,7 @@ object ProjectPlugin extends AutoPlugin {
       addSbtPlugin("com.eed3si9n"       % "sbt-buildinfo"          % "0.7.0"),
       addSbtPlugin("com.geirsson"       % "sbt-scalafmt"           % "0.6.8"),
       addSbtPlugin("com.47deg"          % "sbt-dependencies"       % "0.1.1"),
-      addSbtPlugin("com.47deg"          % "sbt-microsites"         % "0.5.4")
+      addSbtPlugin("com.47deg"          % "sbt-microsites"         % "0.5.6")
     ) ++
       ScriptedPlugin.scriptedSettings ++ Seq(
       scriptedDependencies := (compile in Test) map { _ =>
