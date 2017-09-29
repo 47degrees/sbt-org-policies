a8e2283c21bf30e331a001fa103420c958224229
Juan Pedro Moreno <juanpmorest@gmail.com>
2017-09-29 17:28:17 +0200

    Updates outdated libs. Fix iota organization

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -45,7 +45,7 @@ object ProjectPlugin extends AutoPlugin {
       addSbtPlugin("org.xerial.sbt"     % "sbt-sonatype"           % "2.0"),
       addSbtPlugin("com.jsuereth"       % "sbt-pgp"                % "1.1.0"),
       addSbtPlugin("com.typesafe.sbt"   % "sbt-ghpages"            % "0.6.2"),
-      addSbtPlugin("com.typesafe.sbt"   % "sbt-site"               % "1.3.0"),
+      addSbtPlugin("com.typesafe.sbt"   % "sbt-site"               % "1.3.1"),
       addSbtPlugin("pl.project13.scala" % "sbt-jmh"                % "0.2.27"),
       addSbtPlugin("org.scalastyle"     %% "scalastyle-sbt-plugin" % "1.0.0"),
       addSbtPlugin("org.scoverage"      % "sbt-scoverage"          % "1.5.1"),
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,4 +1,4 @@
 import sbt.Resolver.sonatypeRepo
 
 resolvers ++= Seq(sonatypeRepo("snapshots"), sonatypeRepo("releases"))
-addSbtPlugin("com.47deg" % "sbt-org-policies" % "0.7.2-M1")
+addSbtPlugin("com.47deg" % "sbt-org-policies" % "0.7.5")
