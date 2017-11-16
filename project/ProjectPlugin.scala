283d7ef44bbf6b8a3af25fd0605162c00ce57aec
Fede Fernández <ffbeltran@gmail.com>
2017-11-16 07:52:52 +0100

    Bumps sbt-microsite and sbt-dependencies and upgrades some libraries

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -42,8 +42,8 @@ object ProjectPlugin extends AutoPlugin {
       addSbtPlugin("org.scoverage"      % "sbt-scoverage"          % "1.5.1"),
       addSbtPlugin("org.scala-js"       % "sbt-scalajs"            % "0.6.20"),
       addSbtPlugin("de.heikoseeberger"  % "sbt-header"             % "3.0.1"),
-      addSbtPlugin("com.47deg"          % "sbt-dependencies"       % "0.3.1"),
-      addSbtPlugin("com.47deg"          % "sbt-microsites"         % "0.7.3"),
+      addSbtPlugin("com.47deg"          % "sbt-dependencies"       % "0.3.6"),
+      addSbtPlugin("com.47deg"          % "sbt-microsites"         % "0.7.7"),
       libraryDependencies ++= {
         val sbtBinaryVersionValue = (sbtBinaryVersion in pluginCrossBuild).value
 
