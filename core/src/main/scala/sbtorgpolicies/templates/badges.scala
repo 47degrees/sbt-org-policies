d731deb25b802b48eee1339be4ddc484e69a7982
Juan Pedro Moreno <juanpmorest@gmail.com>
2018-08-07 10:41:33 +0200

    Fixes <= bug

--- core/src/main/scala/sbtorgpolicies/templates/badges.scala
+++ core/src/main/scala/sbtorgpolicies/templates/badges.scala
@@ -120,7 +120,7 @@ object badges {
       val badgeVersion = sjsV match {
         case sjsVSplit(major, minor, patch, other) if ((major == "0") && (minor == "6")) =>
           patch.toInt match {
-            case d if d >= 17 && d < 24 => s"$major.$minor.17"
+            case d if d >= 17 && d <= 24 => s"$major.$minor.17"
             case _                      => sjsV
           }
         case _ => sjsV
