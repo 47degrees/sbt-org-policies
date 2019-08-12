e3d4c768dd985b9628feed2e23e841cae06a54f7
Roberto Serrano <r.semarcos@gmail.com>
2019-08-12 13:00:05 +0200

    Bump Scala 2.12 patch version

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -21,7 +21,7 @@ object ProjectPlugin extends AutoPlugin {
       val cats: String              = "1.6.1"
       val github4s: String          = "0.20.1"
       val moultingyaml: String      = "0.4.0"
-      val scala212: String          = "2.12.8"
+      val scala212: String          = "2.12.9"
       val scala213: String          = "2.13.0"
       val scalacheck: String        = "1.13.5"
       val scalacheckToolbox: String = "0.2.4"
