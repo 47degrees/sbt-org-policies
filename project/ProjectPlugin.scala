ae4d4bd0a445849cab17aa6e4b14160a1fbe9bf9
Scala Steward <me@scala-steward.org>
2020-02-28 12:03:25 +0100

    Update github4s to 0.22.0

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -21,7 +21,7 @@ object ProjectPlugin extends AutoPlugin {
     lazy val V = new {
       val base64: String            = "0.2.9"
       val cats: String              = "2.1.1"
-      val github4s: String          = "0.21.0"
+      val github4s: String          = "0.22.0"
       val moultingyaml: String      = "0.4.1"
       val scala212: String          = "2.12.10"
       val scala213: String          = "2.13.1"
